a5fc5e10a41ce0b9edcb997a5e63c6d52fc84550
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-01-24 14:56:08 +0000

    add a function to check if box has a solution

--- src/box.py
+++ src/box.py
@@ -12,3 +12,10 @@ def box_forward(input_bounds, input_weights, input_biases, apply_relu=True):
     input_biases = np.reshape(input_biases, (-1, 1))
     output_bounds = np.dot(input_weights, input_bounds) + input_biases
     return np.maximum(output_bounds, 0) if apply_relu else output_bounds
+
+
+def box_check_solution(output_bounds, network_output):
+    lower_bound = output_bounds[network_output][0]
+    output_bounds = np.delete(output_bounds, network_output, axis=0)
+    max_upper_bound = np.max(output_bounds, axis=0)[1]
+    return lower_bound > max_upper_bound
