f8a2380637f8064320b0ca817e90cfc987e33753
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-01-24 14:56:08 +0000

    add a function to check if box has a solution

--- test/test_box.py
+++ test/test_box.py
@@ -1,6 +1,6 @@
 import unittest
 from numpy.testing import assert_array_equal
-from src.box import box_forward
+from src.box import box_forward, box_check_solution
 
 
 class TestBox(unittest.TestCase):
@@ -17,6 +17,16 @@ class TestBox(unittest.TestCase):
         input_biases = [0, 2, -1]
         output_bounds = box_forward(input_bounds, input_weights, input_biases, False)
         assert_array_equal(output_bounds, [[4, 6], [-1, 1], [0, 2]])
+
+    def test_box_check_solution(self):
+        output_bounds = [[0.1, 0.4], [0.6, 1.4], [-0.6, 0.2]]
+        network_output = 1
+        has_solution = box_check_solution(output_bounds, network_output)
+        self.assertTrue(has_solution)
+        output_bounds = [[0.1, 0.7], [0.6, 1.4], [-0.6, 0.2]]
+        network_output = 1
+        has_solution = box_check_solution(output_bounds, network_output)
+        self.assertFalse(has_solution)
 
 
 if __name__ == '__main__':
