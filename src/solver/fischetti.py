261b7f61ab12b3f163d3c033da00c23616cbdf77
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-01-27 21:49:12 +0000

    fix inputs

--- src/solver/fischetti.py
+++ src/solver/fischetti.py
@@ -1,6 +1,6 @@
 import numpy as np
 from docplex.mp.model import Model
-from solver_utils import maximize, minimize
+from src.solver.solver_utils import maximize, minimize
 
 
 def build_fischetti_network(layers, variables):
