7fbe66f7d79954db19722e7601b897c3e198e980
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-01-31 19:33:53 +0000

    apply box to the minimal_explication function

--- src/explication/explication_utils.py
+++ src/explication/explication_utils.py
@@ -1,5 +1,7 @@
+import numpy as np
 from src.solver.fischetti import insert_output_constraints_fischetti
 from src.solver.tjeng import insert_tjeng_output_constraints
+from src.explication.box import box_relax_input_bounds, box_has_solution
 
 
 def print_explication(data_index, feature_columns, explanation):
@@ -8,7 +10,7 @@ def print_explication(data_index, feature_columns, explanation):
     print(f'Explication for data {data_index}: {result}')
 
 
-def get_minimal_explication(mdl, bounds, method, network_input, network_output):
+def minimal_explication(mdl, bounds, method, network_input, network_output, layers):
     mdl = mdl.clone()
     number_classes = len(bounds['output'])
     variables = {
@@ -16,18 +18,25 @@ def get_minimal_explication(mdl, bounds, method, network_input, network_output):
         'binary': mdl.binary_var_list(number_classes - 1, name='b')
     }
     input_constraints = mdl.add_constraints(
-        [mdl.get_var_by_name(f'x_{index}') == feature.numpy() for index, feature in enumerate(network_input[0])],
+        [mdl.get_var_by_name(f'x_{index}') == feature for index, feature in enumerate(network_input)],
         names='input')
     mdl.add_constraint(mdl.sum(variables['binary']) >= 1)
     if method == 'fischetti':
         insert_output_constraints_fischetti(mdl, network_output, variables)
     else:
         insert_tjeng_output_constraints(mdl, bounds['output'], network_output, variables)
+    input_mask = np.zeros_like(network_input, dtype=bool)
     for index, constraint in enumerate(input_constraints):
         mdl.remove_constraint(constraint)
+        input_mask[index] = True
+        input_bounds = box_relax_input_bounds(network_input, bounds['input'], input_mask)
+        if box_has_solution(input_bounds, layers, network_output):
+            print(f'Feature {index} is not relevant (using box)')
+            continue
         mdl.solve(log_output=False)
         if mdl.solution is not None:
             mdl.add_constraint(constraint)
+            input_mask[index] = False
             print(f'Feature {index} is relevant (using solver)')
             continue
         print(f'Feature {index} is not relevant (using solver)')
