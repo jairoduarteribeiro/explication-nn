b76b6d958255d0e728d17cc10f78df0f097b249d
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-03-01 12:42:36 +0000

    refactor duplicated code to a function

--- src/models/iris/iris.py
+++ src/models/iris/iris.py
@@ -1,35 +1,11 @@
-from time import time
-from keras.models import Sequential
-from keras.layers import InputLayer, Dense
-from keras.activations import relu, softmax
-from keras.optimizers import Adam
-from keras.losses import SparseCategoricalCrossentropy
-from keras.metrics import SparseCategoricalAccuracy
-from keras.callbacks import EarlyStopping, ModelCheckpoint
-from src.datasets.dataset_utils import get_dataset_path, read_dataset
-from src.models.model_utils import get_model_path
+from src.models.model_utils import train
 
 
 def main():
-    x_train, y_train = read_dataset(get_dataset_path('iris', 'train.csv'), split=True)
-    x_val, y_val = read_dataset(get_dataset_path('iris', 'validation.csv'), split=True)
-    n_classes = 3
-    n_hidden_layers = 4
-    n_neurons = 16
-    n_epochs = 1000
-    batch_size = 4
-    model = Sequential([InputLayer(input_shape=(x_train.shape[1],))])
-    for _ in range(n_hidden_layers - 1):
-        model.add(Dense(n_neurons, activation=relu))
-    model.add(Dense(n_classes, activation=softmax))
-    model.compile(optimizer=Adam(), loss=SparseCategoricalCrossentropy(), metrics=[SparseCategoricalAccuracy()])
-    early_stopping = EarlyStopping(patience=20)
-    model_checkpoint = ModelCheckpoint(filepath=get_model_path('iris', 'iris.h5'), save_best_only=True)
-    start_time = time()
-    model.fit(x_train, y_train, epochs=n_epochs, batch_size=batch_size,
-              validation_data=(x_val, y_val), callbacks=[early_stopping, model_checkpoint])
-    end_time = time()
-    print(f'Time of training: {end_time - start_time:.2f} seconds.')
+    nn_params = {
+        'n_classes': 3, 'n_hidden_layers': 4, 'n_neurons': 16, 'n_epochs': 1000, 'batch_size': 4, 'patience': 100
+    }
+    train('iris', nn_params)
 
 
 if __name__ == '__main__':
--- src/models/model_utils.py
+++ src/models/model_utils.py
@@ -1,5 +1,31 @@
+from time import time
+from keras.models import Sequential
+from keras.layers import InputLayer, Dense
+from keras.activations import relu, softmax
+from keras.optimizers import Adam
+from keras.losses import SparseCategoricalCrossentropy
+from keras.metrics import SparseCategoricalAccuracy
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 from os.path import join, dirname
+from src.datasets.dataset_utils import get_dataset_path, read_dataset
 
 
 def get_model_path(*paths):
     return join(dirname(__file__), *paths)
+
+
+def train(dataset_name, nn_params):
+    x_train, y_train = read_dataset(get_dataset_path(dataset_name, 'train.csv'), split=True)
+    x_val, y_val = read_dataset(get_dataset_path(dataset_name, 'validation.csv'), split=True)
+    model = Sequential([InputLayer(input_shape=(x_train.shape[1],))])
+    for _ in range(nn_params['n_hidden_layers'] - 1):
+        model.add(Dense(nn_params['n_neurons'], activation=relu))
+    model.add(Dense(nn_params['n_classes'], activation=softmax))
+    model.compile(optimizer=Adam(), loss=SparseCategoricalCrossentropy(), metrics=[SparseCategoricalAccuracy()])
+    early_stopping = EarlyStopping(patience=nn_params['patience'])
+    model_checkpoint = ModelCheckpoint(filepath=get_model_path(dataset_name, f'{dataset_name}.h5'), save_best_only=True)
+    start_time = time()
+    model.fit(x_train, y_train, epochs=nn_params['n_epochs'], batch_size=nn_params['batch_size'],
+              validation_data=(x_val, y_val), callbacks=[early_stopping, model_checkpoint])
+    end_time = time()
+    print(f'Time of training: {end_time - start_time:.2f} seconds.')
