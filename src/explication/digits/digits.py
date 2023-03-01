6760b011f2e5597e3a06de5ba4cc6ce5ba2aabf8
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-03-01 13:05:41 +0000

    generate explication to digits dataset

--- src/explication/digits/digits.py
+++ src/explication/digits/digits.py
@@ -2,7 +2,7 @@ from src.explication.explication_utils import get_minimal_explication
 
 
 def main():
-    get_minimal_explication('iris', 'fischetti', True)
+    get_minimal_explication('digits', 'fischetti', True)
 
 
 if __name__ == '__main__':
