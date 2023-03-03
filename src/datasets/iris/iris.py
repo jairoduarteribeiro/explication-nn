cc9eeec5bdd1b54e2e12ac3cf1688950ffebe984
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-03-03 18:13:13 +0000

    apply some refactors

--- src/datasets/iris/iris.py
+++ src/datasets/iris/iris.py
@@ -1,29 +1,15 @@
-import pandas as pd
-from src.datasets.dataset_utils import get_dataset_path, transform, split_dataset, save_dataset
+from sklearn.datasets import load_iris
+from src.datasets.dataset_utils import transform, split_dataset, load_and_save
 
 
-def load_data(split_with_validation=False):
-    path = get_dataset_path('iris', 'iris.csv')
-    column_names = ('id', 'sepal_length_cm', 'sepal_width_cm', 'petal_length_cm', 'petal_width_cm', 'target')
-    df = pd.read_csv(path, header=0, names=column_names)
-    df.loc[df.target == 'Iris-setosa', 'target'] = 0
-    df.loc[df.target == 'Iris-versicolor', 'target'] = 1
-    df.loc[df.target == 'Iris-virginica', 'target'] = 2
-    df['target'] = df['target'].astype('int')
-    x = df.iloc[:, 1:-1]
-    y = df.iloc[:, -1]
+def load_data():
+    x, y = load_iris(as_frame=True, return_X_y=True)
     x = transform(x, x.columns)
-    return split_dataset(x, y, split_with_validation)
+    return split_dataset(x, y, with_validation=True)
 
 
 def main():
-    (x_train, y_train), (x_val, y_val), (x_test, y_test) = load_data(split_with_validation=True)
-    train_csv_path = get_dataset_path('iris', 'train.csv')
-    validation_csv_path = get_dataset_path('iris', 'validation.csv')
-    test_csv_path = get_dataset_path('iris', 'test.csv')
-    save_dataset(x_train, y_train, train_csv_path)
-    save_dataset(x_val, y_val, validation_csv_path)
-    save_dataset(x_test, y_test, test_csv_path)
+    load_and_save('iris', load_data)
 
 
 if __name__ == '__main__':
